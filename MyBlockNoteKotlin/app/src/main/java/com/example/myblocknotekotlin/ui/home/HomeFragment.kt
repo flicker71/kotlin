95ab0ec50ca3bffb59f0a73a23efa4fb5596a86b
Alloula Elias <72727849+flicker71@users.noreply.github.com>
2023-10-17 16:24:53 +0200

    Fonction d'ajout + affichage de liste

--- MyBlockNoteKotlin/app/src/main/java/com/example/myblocknotekotlin/ui/home/HomeFragment.kt
+++ MyBlockNoteKotlin/app/src/main/java/com/example/myblocknotekotlin/ui/home/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.example.myblocknotekotlin.ui.home
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -22,17 +24,30 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val root: View = binding.root
+        val noteEditText = binding.noteEditText
+        val addNoteButton = binding.addNoteButton
+        val notesListView = binding.notesListView
+
+
+        val notes = mutableListOf<String>()
+        val adapter = ArrayAdapter(requireContext(), R.layout.simple_list_item_1, notes)
+        notesListView.adapter = adapter
+
+        addNoteButton.setOnClickListener {
+            val noteText = noteEditText.text.toString()
+            if (noteText.isNotEmpty()) {
+                notes.add(noteText)
+                adapter.notifyDataSetChanged()
+                noteEditText.text.clear()
+            }
         }
+
         return root
+
     }
 
     override fun onDestroyView() {
